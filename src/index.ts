566f7cba46085d289876739516af63737b1de353
koichik <koichik@improvement.jp>
2022-07-08 15:51:05 +0900

    feat: add entry point

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './history/RecoilHistorySyncJSONNext'
+export * from './history/RecoilHistorySyncTransitNext'
+export * from './url/RecoilURLSyncJSONNext'
+export * from './url/RecoilURLSyncTransitNext'
+export * from './utils/atomFamilyWithInitialValue'
