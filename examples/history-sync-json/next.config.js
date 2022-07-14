753d7f556599cbc8e5f30743f46e231af97de394
佐藤昭文 <akfm.sato@gmail.com>
2022-07-15 08:25:57 +0900

    fix: swc minify bug avoidance, update es target

--- examples/history-sync-json/next.config.js
+++ examples/history-sync-json/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   experimental: {
     scrollRestoration: true,
     newNextLinkBehavior: true,
