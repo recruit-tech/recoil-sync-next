e2cb2f033e1271476c06a9a5bde415ebf00c0af9
koichik <koichik@improvement.jp>
2022-07-15 20:31:18 +0900

    examples: use atomWithInitialValue

--- examples/history-sync-json/src/components/Textfield/index.tsx
+++ examples/history-sync-json/src/components/Textfield/index.tsx
@@ -1,17 +1,17 @@
 import { string } from '@recoiljs/refine'
-import { atom, useRecoilState } from 'recoil'
+import { useRecoilState } from 'recoil'
 import { syncEffect } from 'recoil-sync'
+import { atomWithInitialValue } from 'recoil-sync-next'
 
 import styles from './index.module.css'
 
-export const textState = atom<string>({
+export const textState = atomWithInitialValue<string>({
   key: 'textState',
-  default: '',
   effects: [syncEffect({ refine: string() })],
 })
 
 export const Textfield: React.FC = () => {
-  const [text, setText] = useRecoilState(textState)
+  const [text, setText] = useRecoilState(textState('Recoil'))
 
   return (
     <div className={styles.wrapper}>
