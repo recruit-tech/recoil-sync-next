538ae3560068c5874dce8593c284a018cd0fd7db
佐藤昭文 <akfm.sato@gmail.com>
2022-07-21 22:06:33 +0900

    examples: transit history impl add

--- examples/history-sync-json/pages/_app.tsx
+++ examples/history-sync-json/pages/_app.tsx
@@ -1,14 +1,27 @@
 import '../styles/globals.css'
 import { RecoilRoot } from 'recoil'
-import { RecoilHistorySyncJSONNext } from 'recoil-sync-next'
+import { RecoilHistorySyncJSONNext, RecoilHistorySyncTransitNext } from 'recoil-sync-next'
 
 import type { AppProps } from 'next/app'
+import { ViewState } from '../src/components/ViewStateForm'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <RecoilHistorySyncJSONNext storeKey="history-json-store">
-        <Component {...pageProps} />
+        <RecoilHistorySyncTransitNext
+          storeKey="history-transit-store"
+          handlers={[
+            {
+              tag: 'VS',
+              class: ViewState,
+              write: x => [x.active, x.pos],
+              read: ([active, pos]) => new ViewState(active, pos),
+            },
+          ]}
+        >
+          <Component {...pageProps} />
+        </RecoilHistorySyncTransitNext>
       </RecoilHistorySyncJSONNext>
     </RecoilRoot>
   )
--- examples/history-sync-json/pages/index.tsx
+++ examples/history-sync-json/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { Counter } from '../src/components/Counter'
 import { Links } from '../src/components/Links'
 import { Textfield } from '../src/components/Textfield'
+import { ViewStateForm } from '../src/components/ViewStateForm'
 import styles from '../styles/Home.module.css'
 
 import type { NextPage } from 'next'
@@ -17,11 +18,24 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Top page (Static)</h1>
-        <Textfield />
-        <Counter name="foo" initialValue={0} />
-        <Counter name="bar" initialValue={10} />
-        <Counter name="baz" initialValue={100} />
-        <Links />
+        <div>
+          <h2>Textfield</h2>
+          <Textfield />
+        </div>
+        <div>
+          <h2>Counter</h2>
+          <Counter name="foo" initialValue={0} />
+          <Counter name="bar" initialValue={10} />
+          <Counter name="baz" initialValue={100} />
+        </div>
+        <div>
+          <h2>ViewState</h2>
+          <ViewStateForm />
+        </div>
+        <div>
+          <h2>Links</h2>
+          <Links />
+        </div>
       </main>
     </div>
   )
