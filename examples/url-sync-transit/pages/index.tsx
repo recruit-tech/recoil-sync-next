ce1f81ba33cf96aeef61cc820a241b556ebd0e70
佐藤昭文 <akfm.sato@gmail.com>
2022-07-24 22:18:58 +0900

    examples: url-sync-transit init

---
+++ examples/url-sync-transit/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { Counter } from '../src/components/Counter'
+import { Links } from '../src/components/Links'
+import { Textfield } from '../src/components/Textfield'
+import styles from '../styles/Home.module.css'
+
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Top - Example of RecoilURLSyncJSONNext</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Top page (Static)</h1>
+        <Textfield />
+        <Counter name="foo" initialValue={0} />
+        <Counter name="bar" initialValue={10} />
+        <Counter name="baz" initialValue={100} />
+        <Links />
+      </main>
+    </div>
+  )
+}
+
+export default Home
