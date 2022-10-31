64bcebc969d372d7f83c991b0eee0ede528ca924
koichik <koichik@improvement.jp>
2022-10-31 12:11:49 +0900

    examples: remove swcMinify & newNextLinkBehavior as it's the new default

--- examples/history-sync-json/next.config.js
+++ examples/history-sync-json/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
-    scrollRestoration: true,
     newNextLinkBehavior: true,
   },
 }
