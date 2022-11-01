7808b437ba1592a387f4dc6eaa3159f3ce350449
Koichi Kobayashi <koichik@improvement.jp>
2022-11-01 22:35:02 +0900

    examples: Remove swcMinify and newNextLinkBehavior as they're the new default for Next.js 13 (#137)
    * examples: update dependencies
    
    * examples: remove swcMinify & newNextLinkBehavior as it's the new default

--- examples/url-sync-json/next.config.js
+++ examples/url-sync-json/next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     scrollRestoration: true,
-    newNextLinkBehavior: true,
   },
 }
 
